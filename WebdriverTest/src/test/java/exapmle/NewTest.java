0c69e2b15b49908c74ebde6d949d67cdd631c6f5
Anna <helloboysandgirls.az@gmail.com>
2018-03-09 06:53:50 -0500

    Update NewTest.java

--- WebdriverTest/src/test/java/exapmle/NewTest.java
+++ WebdriverTest/src/test/java/exapmle/NewTest.java
@@ -22,7 +22,7 @@ public class NewTest {
   @BeforeTest
   public void beforeTest() {
 	//  System.setProperty("webdriver.chrome.driver","C:\\Users\\anna.zhuravleva\\Documents\\Alrquitectura\\WebDrivers\\Chrome Driver\\chromedriver.exe"); //my windows path
-	 System.setProperty("webdriver.chrome.driver","chromedriver"); //relative path windows
+	 System.setProperty("webdriver.chrome.driver","..//chromedriver"); //relative path windows
 	 // System.setProperty("webdriver.chrome.driver","//tmp//jenkins//tools//chromedriver"); //path linux for jenkins chromedriver
 	  driver = new ChromeDriver();
 	  
