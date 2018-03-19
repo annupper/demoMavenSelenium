0bb31eca087b1ad0305f8059fc338626fc0bb533
Anna <helloboysandgirls.az@gmail.com>
2018-03-19 07:45:08 -0400

    Update NewTest.java
    changes for phantomjs

--- WebdriverTest/src/test/java/exapmle/NewTest.java
+++ WebdriverTest/src/test/java/exapmle/NewTest.java
@@ -11,14 +11,15 @@ import org.testng.annotations.AfterTest;
 
 public class NewTest {
 	private WebDriver driver;
+	private String baseUrl;
 	
   @Test
   public void testEasy() {
 	 // System.setProperty("webdriver.gecko.driver","C:\\Users\\anna.zhuravleva\\Documents\\Alrquitectura\\WebDrivers\\Mozilla Firefox\\geckodriver.exe");
-	  	
-	  	//String baseUrl = "https://www.google.com";
-	  	//driver.get(baseUrl);
+
 	  System.out.println("TEST");
+	  baseUrl = "https://www.google.com";
+	  driver.get(baseUrl);
   }
   @BeforeTest
   public void beforeTest() {
@@ -27,13 +28,21 @@ public class NewTest {
 	 // System.setProperty("webdriver.chrome.driver","//tmp//jenkins//tools//chromedriver"); //path linux for jenkins chromedriver
 	 // driver = new ChromeDriver();
 	  System.out.println("Starting Before TEST");
+	  DesiredCapabilities caps = new DesiredCapabilities();
+caps.setJavascriptEnabled(true);                
+caps.setCapability("takesScreenshot", true);  
+caps.setCapability(
+                        PhantomJSDriverService.PHANTOMJS_EXECUTABLE_PATH_PROPERTY,
+                        "//usr//local//bin/phantomjs"
+                    );
+driver = new  PhantomJSDriver(caps);
 		
   }
 
   @AfterTest
   public void afterTest() {
-	  //driver.quit();
 	  System.out.println("Starting After TEST");
+	  driver.quit();
   }
 
 }
