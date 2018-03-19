077c948d192284fd9d28eb18ac97f7441a3285c5
Anna <helloboysandgirls.az@gmail.com>
2018-03-19 13:02:59 +0100

    Update NewTest.java

--- WebdriverTest/src/test/java/exapmle/NewTest.java
+++ WebdriverTest/src/test/java/exapmle/NewTest.java
@@ -11,15 +11,14 @@ import org.testng.annotations.AfterTest;
 
 public class NewTest {
 	private WebDriver driver;
-	private String baseUrl;
+	
 	
   @Test
   public void testEasy() {
 	 // System.setProperty("webdriver.gecko.driver","C:\\Users\\anna.zhuravleva\\Documents\\Alrquitectura\\WebDrivers\\Mozilla Firefox\\geckodriver.exe");
 
 	  System.out.println("TEST");
-	  baseUrl = "https://www.google.com";
-	  driver.get(baseUrl);
+	  
   }
   @BeforeTest
   public void beforeTest() {
@@ -28,21 +27,12 @@ public class NewTest {
 	 // System.setProperty("webdriver.chrome.driver","//tmp//jenkins//tools//chromedriver"); //path linux for jenkins chromedriver
 	 // driver = new ChromeDriver();
 	  System.out.println("Starting Before TEST");
-	  DesiredCapabilities caps = new DesiredCapabilities();
-caps.setJavascriptEnabled(true);                
-caps.setCapability("takesScreenshot", true);  
-caps.setCapability(
-                        PhantomJSDriverService.PHANTOMJS_EXECUTABLE_PATH_PROPERTY,
-                        "//usr//local//bin/phantomjs"
-                    );
-driver = new  PhantomJSDriver(caps);
 		
   }
 
   @AfterTest
   public void afterTest() {
 	  System.out.println("Starting After TEST");
-	  driver.quit();
   }
 
 }
